bf3c0ae509d47bd607d1752d5c2143245d25dbfd
Florian Bordignon <florian.bordignonstage@decathlon.com>
2023-03-09 13:51:20 +0100

    add project

---
+++ tuto_spring_docker/app/src/main/java/com/example/tuto_spring_docker/TutoSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tuto_spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoSpringDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoSpringDockerApplication.class, args);
+    }
+
+}
---
+++ tuto_spring_docker/app/src/test/java/com/example/tuto_spring_docker/TutoSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tuto_spring_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoSpringDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
